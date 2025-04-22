b0543e365a9b24bf3b97472674076e413a4023da
Saurav <palsaurav25@gmail.com>
2025-04-23 00:04:34 +0530

    Added local peerjs server

--- frontend/src/Context/SocketContect.tsx
+++ frontend/src/Context/SocketContect.tsx
@@ -45,7 +45,12 @@ export const SocketProvider: React.FC<Props> = ({children}) => {
     useEffect(()=>{
 
         const userId = UUIDv4();
-        const newPeer = new Peer(userId);
+        const newPeer = new Peer(userId,{
+            host:"localhost",
+            port:9000,
+            path:'/myapp'
+        });
+
     
         setUser(newPeer);
         fetchUserFeed();
