c1da876b8bf33de55f7276890217f42b46ce6c88
Saurav <palsaurav25@gmail.com>
2025-04-25 02:10:25 +0530

    updated remote stream code

--- src/Components/UserFeedPlayer.tsx
+++ src/Components/UserFeedPlayer.tsx
@@ -1,26 +1,26 @@
 import { useEffect, useRef } from "react";
 
-const UserFeedPlayer:React.FC<{stream?:MediaStream}> = ({stream})=>{
-
-
-
-
+const UserFeedPlayer: React.FC<{ stream?: MediaStream }> = ({ stream }) => {
     const videoRef = useRef<HTMLVideoElement>(null);
-    useEffect(()=>{
-        if(videoRef.current&&stream){
-            videoRef.current.srcObject=stream;
+
+    useEffect(() => {
+        if (videoRef.current && stream) {
+            console.log("Setting stream to video", stream);
+            console.log("Stream tracks:", stream.getTracks()); // Debugging track
+            videoRef.current.srcObject = stream;
         }
-    },[stream]);
-    
-    
-    return(
+    }, [stream]);
+
+    return (
         <video
             ref={videoRef}
-            style={{width:'300px',height:'200'}}
+            style={{ width: '300px', height: 200 }}
             muted={true}
             autoPlay
+            playsInline
         />
-    )
+    );
 };
+
 
 export default UserFeedPlayer;
